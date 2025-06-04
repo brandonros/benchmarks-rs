51845addc411244e94c95dc423e90c42cd1e6f8a
Brandon Ros <brandonros1@gmail.com>
2025-06-03 22:39:33 -0400

    blackbox

--- benches/base58.rs
+++ benches/base58.rs
@@ -9,18 +9,17 @@ fn bench_bs58_encode(c: &mut Criterion) {
         .try_into()
         .unwrap();
     
-    let mut group = c.benchmark_group("bs58_encoding_custom");
+    let mut group = c.benchmark_group("base58");
     
     // Configure sample size and measurement time
     group.sample_size(1000);
     group.measurement_time(std::time::Duration::from_secs(10));
     group.throughput(Throughput::Bytes(32));
     
-    group.bench_function("bs58_encode_custom", |b| {
+    group.bench_function("base58_encode32", |b| {
         b.iter(|| {
             let mut bs58_encoded_public_key = [0u8; 64];
-            let encoded_len = base58::base58_encode32(black_box(&public_key_bytes), &mut bs58_encoded_public_key);
-            let _ = &bs58_encoded_public_key[0..encoded_len];
+            let _encoded_len = black_box(base58::base58_encode32(&public_key_bytes, &mut bs58_encoded_public_key));
         })
     });
     
